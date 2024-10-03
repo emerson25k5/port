9e08e06efff0acdd87f43db7553e2ee996a148ad
emerson25k5 <emerson25k5@gmail.com>
2024-10-03 19:58:43 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Emerson DevWeb</title>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+  <section id="hero">
+    <h1>Emerson Santos</h1>
+    <p>Desenvolvedor Web focado em criar experiências digitais modernas.</p>
+  </section>
+
+  <section id="sobre">
+    <h2>Sobre mim</h2>
+    <p>Formado em desenvolvimento de sistemas web com experiência em projetos front-end e back-end. Experiência com PHP, MySQL, Laravel, Bootstrap, dentre outras tecnologias.</p>
+  </section>
+
+  <section id="projetos">
+    <h2>Projetos</h2>
+    <div class="projetos-container">
+      <div class="projeto-card">
+      <img src="imagens/proj_autcert.png" alt="projeto_autcert" style="width:100%;border-radius:8px;">
+        <h3>AutCert</h3>
+        <p>Ferramenta para gestão de eventos e geração de certificados de forma automática</p>
+        <a href="https://swupe.com.br/autcert" target="_blank">Ver Projeto</a>
+      </div>
+      <div class="projeto-card">
+      <img src="imagens/proj_desp.png" alt="projeto_autcert" style="width:100%;border-radius:8px;">
+        <h3>Despachante Rodriboy</h3>
+        <p>Landpage para um despachante, lista de serviços, formulário para contato e API do Google Maps</p>
+        <a href="https://swupe.com.br/desp" target="_blank">Ver Projeto</a>
+      </div>
+      <div class="projeto-card">
+      <img src="imagens/proj_swupe.png" alt="projeto_autcert" style="width:100%;border-radius:8px;">
+        <h3>Swupe</h3>
+        <p>Sistema Web para gestão de funcionários, controle de uniformes dos funcionários, férias, trabalhos avulsos, entre outros</p>
+        <a href="https://swupe.com.br/swupe" target="_blank">Ver Projeto</a>
+      </div>
+      <!-- Adicione mais projetos conforme necessário -->
+    </div>
+    <br>
+    <div class="projetos-container">
+      <div class="projeto-card">
+      <img src="imagens/proj_melb.png" alt="projeto_autcert" style="width:100%;border-radius:8px;">
+        <h3>Melb Confecções</h3>
+        <p>Landpage para a marca de vestimentas infantis utilizando o framework <a href="https://materializecss.com/" target="_blank">Materialize</a> no frontend</p>
+        <a href="https://swupe.com.br/melb" target="_blank">Ver Projeto</a>
+      </div>
+      <div class="projeto-card">
+      <img src="imagens/proj_melmi.png" alt="projeto_autcert" style="width:100%;border-radius:8px;">
+        <h3>Melmi Açaí</h3>
+        <p>Sistema Web para gerenciamento de pedidos de uma açaiteria. <b>Ainda em desenvolvimento.</b></p>
+        <a href="https://swupe.com.br/melmi" target="_blank">Ver Projeto</a>
+      </div>
+      <div class="projeto-card">
+      
+        <h3>Phoenix</h3>
+        <p>Controle de estoque desenvolvido com Laravel e MySql. <b>Ainda em desenvolvimento.</b></p>
+        <a href="[Link do projeto]" target="_blank">Ver Projeto</a>
+      </div>
+      <!-- Adicione mais projetos conforme necessário -->
+    </div>
+  </section>
+
+
+  <section id="tecnologias">
+    <h2>Tecnologias</h2>
+    <div class="tecnologias-container">
+      <div class="tecnologia">
+        <img class="tecnologia" src="imagens/bootstrap_icon.png" alt="projeto_autcert"">
+      </div>
+      <div class="tecnologia">
+        <img class="tecnologia" src="imagens/mysql_icon.png" alt="projeto_autcert">
+      </div>
+      <div class="tecnologia">
+        <img class="tecnologia" src="imagens/php_icon.png" alt="projeto_autcert">
+      </div>
+      <div class="tecnologia">
+        <img class="tecnologia" src="imagens/laravel_icon.png" alt="projeto_autcert">
+      </div>
+      <div class="tecnologia">
+        <img class="tecnologia" src="imagens/docker_icon.png" alt="projeto_autcert">
+      </div>
+      <!-- Adicione mais projetos conforme necessário -->
+    </div>
+  </section>
+
+  <section id="contato" >
+    <h2>Contato</h2>
+    <p>Interessado em colaborar ou tem uma vaga disponível? Entre em contato comigo!</p>
+    <a href="mailto:emerson25k5@gmail.com">emerson25k5@gmail.com</a>
+  </section>
+
+  <footer>
+    <p>© 2024 Emerson - Todos os direitos reservados.</p>
+  </footer>
+</body>
+</html>
