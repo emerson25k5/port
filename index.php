7b19c0f7a135a03573dd29ebdbdcf70cb01ec05e
emerson25k5 <110756033+emerson25k5@users.noreply.github.com>
2025-03-25 18:06:39 -0300

    add meta tags, update project descriptions, and correct footer year

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Desenvolvedor Web focado em criar experiências digitais modernas.">
+  <meta name="keywords" content="Desenvolvedor Web, Web Developer, PHP, Laravel, MySQL, HTML5, CSS, Bootstrap, Git, GitHub, Docker, Redis">
+  <meta name="author" content="Emerson Santos">
+  <meta name="robots" content="index, follow">
+  <meta name="revisit-after" content="7 days">
+  <meta name="theme-color" content="#333333">
+  <link rel="icon" href="imagens/icons/swupe_icon.png" type="image/png">
+  <meta property="og:title" content="Emerson DevWeb">
   <title>Emerson DevWeb</title>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -40,40 +48,98 @@
       <img src="imagens/proj_autcert.png" alt="projeto_autcert" style="width:100%;border-radius:8px;">
         <h3>AutCert</h3>
         <p>Ferramenta para gestão de eventos e geração de certificados</p>
-        <a href="https://swupe.com.br/autcert" target="_blank">Ver Projeto</a>
+        <h6>Tecnologias:</h6>
+        <ul class="tecnologias">
+          <li>PHP</li>
+          <li>MySQL</li>
+          <li>HTML5</li>
+          <li>CSS</li>
+          <li>Bootstap</li>
+          <li>Git</li>
+          <li>GitHub</li>
+          <li>Redis</li>
+          <li>TCPDF</li>
+        </ul>
+        <a href="https://encodify.com.br/autcert" target="_blank">Ver Projeto</a>
       </div>
       <div class="projeto-card">
       <img src="imagens/proj_desp.png" alt="projeto_autcert" style="width:100%;border-radius:8px;">
-        <h3>Despachante Rodriboy</h3>
-        <p>Landpage para um despachante, lista de serviços, formulário para contato e API do Google Maps</p>
-        <a href="https://swupe.com.br/desp" target="_blank">Ver Projeto</a>
+        <h3>Landpage para Despachante</h3>
+        <p>Landpage para um despachante, lista de serviços, formulário para contato e integração com API do Google Maps</p>
+          <h6>Tecnologias:</h6>
+          <ul class="tecnologias">
+            <li>PHP</li>
+            <li>HTML5</li>
+            <li>CSS</li>
+            <li>Bootstap</li>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>Google Maps API</li>
+          </ul>
+        <a href="https://encodify.com.br/desp" target="_blank">Ver Projeto</a>
       </div>
       <div class="projeto-card">
       <img src="imagens/proj_swupe.png" alt="projeto_autcert" style="width:100%;border-radius:8px;">
-        <h3>Swupe</h3>
+        <h3>Sistema para gestão de funcionários</h3>
         <p>Sistema Web para gestão de funcionários, controle de uniformes dos funcionários, férias, trabalhos avulsos, entre outros</p>
-        <a href="https://swupe.com.br/swupe" target="_blank">Ver Projeto</a>
+          <ul class="tecnologias">
+            <li>PHP</li>
+            <li>HTML5</li>
+            <li>CSS</li>
+            <li>Materialize</li>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>MySQL</li>
+          </ul>
+        <a href="https://encodify.com.br/swupe" target="_blank">Ver Projeto</a>
       </div>
-      <!-- Adicione mais projetos conforme necessário -->
     </div>
+
     <br>
+
     <div class="projetos-container">
       <div class="projeto-card">
       <img src="imagens/proj_melb.png" alt="projeto_autcert" style="width:100%;border-radius:8px;">
         <h3>Melb Confecções</h3>
         <p>Landpage para a marca de vestimentas infantis utilizando o framework <a href="https://materializecss.com/" target="_blank">Materialize</a> no frontend</p>
-        <a href="https://swupe.com.br/melb" target="_blank">Ver Projeto</a>
+          <ul class="tecnologias">
+            <li>PHP</li>
+            <li>HTML5</li>
+            <li>CSS</li>
+            <li>Materialize</li>
+            <li>Git</li>
+            <li>GitHub</li>
+          </ul>
+        <a href="https://encodify.com.br/melb" target="_blank">Ver Projeto</a>
       </div>
       <div class="projeto-card">
       <img src="imagens/proj_melmi.png" alt="projeto_autcert" style="width:100%;border-radius:8px;">
         <h3>Melmi Açaí</h3>
         <p>Sistema Web para gerenciamento de pedidos de uma açaiteria. <b>Ainda em desenvolvimento.</b></p>
-        <a href="https://swupe.com.br/melmi" target="_blank">Ver Projeto</a>
+          <ul class="tecnologias">
+            <li>PHP</li>
+            <li>HTML5</li>
+            <li>CSS</li>
+            <li>Bootstap</li>
+            <li>Git</li>
+            <li>GitHub</li>
+          </ul>
+        <a href="https://encodify.com.br/melmi" target="_blank">Ver Projeto</a>
       </div>
       <div class="projeto-card">
       
         <h3>Phoenix</h3>
         <p>Controle de estoque desenvolvido com Laravel e MySql. <b>Ainda em desenvolvimento.</b></p>
+          <ul class="tecnologias">
+            <li>Laravel</li>
+            <li>PHP</li>
+            <li>HTML5</li>
+            <li>CSS</li>
+            <li>Bootstap</li>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>MySQL</li>
+          </ul>
         <a href="[Link do projeto]" target="_blank">Ver Projeto</a>
       </div>
       <!-- Adicione mais projetos conforme necessário -->
@@ -99,6 +165,12 @@
       <div class="tecnologia">
         <img class="tecnologia" src="imagens/icons/docker_icon.png" alt="projeto_autcert">
       </div>
+      <div class="tecnologia">
+        <img class="tecnologia" src="imagens/icons/git_icon.png" alt="projeto_autcert">
+      </div>
+      <div class="tecnologia">
+        <img class="tecnologia" src="imagens/icons/redis_icon.png" alt="projeto_autcert">
+      </div>
       <!-- Adicione mais projetos conforme necessário -->
     </div>
   </section>
@@ -110,7 +182,7 @@
   </section>
 
   <footer>
-    <p>© 2024 Emerson - Todos os direitos reservados.</p>
+    <p>© 2025 Encodify - Todos os direitos reservados.</p>
   </footer>
 </body>
 </html>
